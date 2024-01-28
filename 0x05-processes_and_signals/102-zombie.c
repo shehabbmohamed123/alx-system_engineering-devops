83faa98d752d22602d16c828cc9feba957dea994
shehabbmohamed123 <shehab12p@gmail.com>
2024-01-28 10:22:21 -0800

    a

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,14 +1,15 @@
+nclude <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
- /**
-    * infinite_while - Run an infinite while loop.
-     *
-      * Return: Always 0.
-       */
- int infinite_while(void)
+/**
+   * infinite_while - Run an infinite while loop.
+    *
+     * Return: Always 0.
+      */
+int infinite_while(void)
 {
 		while (1)
 				{
