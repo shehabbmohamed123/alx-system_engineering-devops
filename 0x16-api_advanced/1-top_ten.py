4402c88b1d0ca3eebdeabd36d2980c1086d4665e
shehabbmohamed123 <shehab12p@gmail.com>
2024-05-07 15:31:15 -0700

    a

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
 
-"""
-prints the titles of the first 10 hot posts listed for a given subreddit
-"""
-
-from requests import get
-
+import praw
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
+    reddit = praw.Reddit(client_id='your_client_id',
+                         client_secret='your_client_secret',
+                         user_agent='your_user_agent')
 
     try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+        subreddit = reddit.subreddit(subreddit)
+        for submission in subreddit.hot(limit=10):
+            print(submission.title)
+    except praw.exceptions.InvalidSubreddit:
         print("None")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
